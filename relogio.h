6b5719f5b24b7564fb8413dbcd82fd22208b8da5
Tiago <161158476+Tiago70@users.noreply.github.com>
2025-12-02 22:11:32 -0300

    Add relogio.h for RTC class definition

---
+++ relogio.h
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#ifndef __RELOGIO_
+#define __RELOGIO_
+
+#include <Arduino.h>
+#include <Ds1302.h>
+
+class relogioObj{
+private:
+  Ds1302* rtc = nullptr;
+
+public:
+  void begin(int CE, int CLK, int IO);
+  String getTempoString();
+  String formatarSeg(uint32_t segundos);
+  uint32_t timestamp();
+};
+
+extern relogioObj relogio;
+
+#endif
